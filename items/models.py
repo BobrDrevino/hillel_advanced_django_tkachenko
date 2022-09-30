ef4fe0bc34f04ccc3c42cce98feb2b113f335a3a
Ilya Tkachenko <tv.ilya1997@gmail.com>
2022-09-30 14:38:25 +0300

    created model and migrate

--- items/models.py
+++ items/models.py
@@ -1,3 +1,33 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    price = models.PositiveIntegerField()
+    sku = models.CharField(max_length=255)
+
+
+class Discount(models.Model):
+    CHOICES = [
+        (0, 'В деньгах'),
+        (1, 'В процентах')
+    ]
+    amount = models.PositiveIntegerField()
+    code = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    discount_type = models.CharField(choices=CHOICES, max_length=255, default=1)
+    
