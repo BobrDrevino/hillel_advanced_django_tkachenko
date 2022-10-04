208146718ebe812068a74f2695e7a7ffc5637e13
Ilya Tkachenko <tv.ilya1997@gmail.com>
2022-10-04 11:27:57 +0300

    amount with discount

--- orders/admin.py
+++ orders/admin.py
@@ -3,12 +3,10 @@ from orders.models import Order, Discount
 
 
 @admin.register(Order)
-class ItemAdmin(admin.ModelAdmin):
-    # list_display = ('name', 'created_at')
+class OrderAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
 
 
 @admin.register(Discount)
-class ItemAdmin(admin.ModelAdmin):
-    # list_display = ('name', 'created_at')
+class DiscountAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
