6327975828b72ed4017b5e45bf14130eb355e228
Ilya Tkachenko <tv.ilya1997@gmail.com>
2022-10-09 18:20:19 +0300

    Add app feedbacks.

--- feedbacks/migrations/0002_initial.py
+++ feedbacks/migrations/0002_initial.py
@@ -19,12 +19,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Feedback',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(default=uuid.uuid4,
+                                        editable=False,
+                                        primary_key=True,
+                                        serialize=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('text', models.TextField()),
-                ('rating', models.PositiveSmallIntegerField(validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(5)])),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('rating', models.PositiveSmallIntegerField(validators=[django.core.validators.MinValueValidator(1),    # noqa
+                                                                        django.core.validators.MaxValueValidator(5)])), # noqa
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),  # noqa
             ],
             options={
                 'abstract': False,
