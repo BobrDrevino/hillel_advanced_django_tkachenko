d7343f470c172e9018363fdfa73410dbba807a70
Ilya Tkachenko <tv.ilya1997@gmail.com>
2022-10-19 09:24:08 +0300

    Products app.

--- products/admin.py
+++ products/admin.py
@@ -1,27 +1,22 @@
 from django.contrib import admin
-from items.models import Item, Product, Category
 from django.utils.safestring import mark_safe
 
+from products.models import Category, Product
 
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('item_image', 'name', 'created_at')
-    list_filter = ('created_at',)
 
-    def item_image(self, obj):
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    filter_horizontal = ('products',)
+    list_display = ('name', 'price', 'sku', 'created_at')
+    list_filter = ('price', )
+    readonly_fields = ('id',)
+
+    def product_image(self, obj):
         if obj.image:
             return mark_safe((
                 '<img src="{}" width="64" height="64" />'.format(
                     obj.image.url)))
         return ''
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    filter_horizontal = ('items',)
-    list_display = ('name', 'price', 'sku', 'created_at')
-    list_filter = ('price', )
-    readonly_fields = ('id',)
 
 
 @admin.register(Category)
