1225e616e4c5beaf09e9c0affe9f096d51fc433e
Ilya Tkachenko <tv.ilya1997@gmail.com>
2022-10-20 11:35:39 +0300

    style not working

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from products.views import ProductsView
+from products.views import ProductsView, ProductDetail
 
 urlpatterns = [
     path('products', ProductsView.as_view(), name='products'),
+    path('products/<uuid:pk>/', ProductDetail.as_view(),
+         name='product_detail'),
 ]
